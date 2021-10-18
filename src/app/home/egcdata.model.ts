3aa5242b4de87edd51a4c983323a117e3b2c5bb4
Jared Hodgkins <phusiondev@gmail.com>
2021-10-18 13:04:25 -0400

    Initial Commit - Working Application

---
+++ src/app/home/egcdata.model.ts
@@ -0,0 +1,8 @@
+export interface EgcData {
+  totalSupply: number;
+  burnedTokens: number;
+  circulatingSupply: number;
+  rewardPercent: number;
+  dailyVolume: number;
+  egcHeld: number;
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EgcData } from './egcdata.model';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,70 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  egcData: EgcData = {
+    totalSupply: 1000000000000000,
+    burnedTokens: 514725329117038,
+    circulatingSupply: 1000000000000,
+    rewardPercent: 0.08,
+    dailyVolume: 1000000,
+    egcHeld: 5000000000,
+  };
 
-  constructor() {}
+  public rewards = 0;
 
+  constructor() {
+    this.updateCirculatingSupply();
+    this.calculateRewards();
+  }
+
+  updateCirculatingSupply() {
+    this.egcData.circulatingSupply =
+      this.egcData.totalSupply - this.egcData.burnedTokens;
+  }
+
+  calculateRewards() {
+    const totalDistribution = this.totalDistribution();
+    const effectivePercentage = this.effectivePercentage();
+    this.rewards = effectivePercentage * totalDistribution;
+  }
+
+  totalDistribution() {
+    return this.egcData.dailyVolume * this.egcData.rewardPercent;
+  }
+
+  effectivePercentage() {
+    return this.egcData.egcHeld / this.egcData.circulatingSupply;
+  }
+
+  // onChange Methods
+  onChangeTokensBurned(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    const parsedValue = parseFloat(value);
+
+    if (!isNaN(parsedValue)) {
+      this.egcData.burnedTokens = parsedValue;
+      this.updateCirculatingSupply();
+      this.calculateRewards();
+    }
+  }
+
+  onChangeDailyVolume(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    const parsedValue = parseFloat(value);
+
+    if (!isNaN(parsedValue)) {
+      this.egcData.dailyVolume = parsedValue;
+      this.calculateRewards();
+    }
+  }
+
+  onChangeEgcHeld(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    const parsedValue = parseFloat(value);
+
+    if (!isNaN(parsedValue)) {
+      this.egcData.egcHeld = parsedValue;
+      this.calculateRewards();
+    }
+  }
 }
